54ddc7301b621a31cd0f29c8964c8ba503266e3e
Broda Noel <brodanoel@gmail.com>
2017-07-04 00:36:19 -0300

    Add Firebase Functions: CORS and Authenticated Endpoint

--- README.md
+++ README.md
@@ -1,23 +1,31 @@
 # Firebase App Example
-This repo is created to improve knowledge about Firebase and do some tests.
-
-URL: https://fir-tests-f81de.firebaseapp.com
-
+This repo is created to improve knowledge about Firebase and do some tests. Enjoy it!
 
 ## Features
 
-### Init Firebase
+### Frontend
+#### Init Firebase
 Just a basic Firebase initialization, necessary before start doing anything. Do not forget to check the `package.json` file to see all necessary dependencies.
 > Search in this project for: `// Feature: Init Firebase`
 
-### Keep Firebase Token updated in LocalStorage.
+#### Keep Firebase Token updated in LocalStorage.
 Every time that the Firebase Token change (because the token expired or the user login or logout), this token will be written in localStorage, as `firebaseToken` key.
 > Search in this project for: `// Feature: Keep Firebase Token Updated`
 
-### Keep on sync a var that is defined in the Firebase Realtime Database.
+#### Keep on sync a var that is defined in the Firebase Realtime Database.
 I defined a var in the realtime database, and this var will be shown and changed automatically every time the var change its value in the database.
 > Search in this project for: `// Feature: Keep var on sync`
 
-### Login with Twitter
+#### Login with Twitter
 This site allows you to login using Twitter, with the Firebase Authentication service.
 > Search in this project for: `// Feature: Login with Twitter`
+
+### Backend (functions)
+#### Endpoint with Authentication
+This endpoint is created to just say Hello! using GET method and only for authenticated users.
+The endpoint is exposed in: `https://us-central1-fir-tests-f81de.cloudfunctions.net/api/sayHelo`
+> Search in this project for: `// Feature: Hello endpoint with authentication`
+
+#### Fix CORS issues
+An configuration to avoid CORS issues for every origin.
+> Search in this project for: `// Feature: CORS`
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,57 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const express = require('express');
+// Feature: CORS
+const cors = require('cors')({origin: true});
+const app = express();
+
+// Feature: CORS
+app.use(cors);
+
+// Feature: Hello endpoint with authentication
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const validateFirebaseIdToken = (req, res, next) => {
+  if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer '))) {
+    res.status(403).send({
+      status: 'error',
+      error: {
+        id: 'app/token-needed',
+        message: 'You need to send a Token'
+      }
+    });
+    return;
+  }
+
+  const idToken = req.headers.authorization.split('Bearer ')[1];
+
+  admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
+    req.user = decodedIdToken;
+    next();
+  }).catch(error => {
+    res.status(403).send({
+      status: 'error',
+      error: {
+        id: 'app/token-invalid',
+        message: 'You need to send a *valid* Token'
+      }
+    });
+  });
+};
+
+// Feature: Hello endpoint with authentication
+app.use(validateFirebaseIdToken);
+
+// Feature: Hello endpoint with authentication
+app.get('/sayHello', (req, res) => {
+  res.send({
+    data: 'Hello!'
+  });
+});
+
+exports.api = functions.https.onRequest(app);
