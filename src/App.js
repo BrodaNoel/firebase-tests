3124b37ef18c023bcabd7a4c9acb6fe6d4d09c7b
Broda Noel <brodanoel@gmail.com>
2017-07-03 22:17:52 -0300

    Implement Options and LoginWithTwitter containers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import LoginWithTwitter from 'containers/LoginWithTwitter';
+import Options from 'containers/Options';
 import './App.css';
 
 class App extends Component {
+  state = {
+    route: 'Options'
+  };
+
+  changeRoute = route => {
+    this.setState({ route });
+  }
+
   render() {
     return (
       <div className="App">
-        Firebase testing app
+        <h1>Firebase testing app</h1>
+
+        <div className="content">
+          {
+            (route => {
+              switch (route) {
+                case 'LoginWithTwitter':
+                  return <LoginWithTwitter></LoginWithTwitter>;
+                case 'Options':
+                default:
+                  return <Options changeRoute={this.changeRoute}></Options>;
+              }
+            })(this.state.route)
+          }
+        </div>
       </div>
     );
   }
