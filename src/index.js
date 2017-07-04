b2a3677a13a9e35bf7bb9ffdc4dc5d05603f13cb
Broda Noel <brodanoel@gmail.com>
2017-07-03 21:59:43 -0300

    Add dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyDMc_FXTVNCIZ-Gik9ccrzpoMgcEIxxzfs",
+  authDomain: "fir-tests-f81de.firebaseapp.com",
+  databaseURL: "https://fir-tests-f81de.firebaseio.com",
+  projectId: "fir-tests-f81de",
+  storageBucket: "fir-tests-f81de.appspot.com",
+  messagingSenderId: "431141611990"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
